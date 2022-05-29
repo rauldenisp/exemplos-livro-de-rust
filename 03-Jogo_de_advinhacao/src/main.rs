f76b8c87be00d355cc4b1159be480ffe5754abff
Raul Denis Pickcius <rauldenisp@yahoo.com>
2022-05-29 20:28:57 -0300

    03 - Tratando entradas inválidas Removendo o print de teste

--- 03-Jogo_de_advinhacao/src/main.rs
+++ 03-Jogo_de_advinhacao/src/main.rs
@@ -9,8 +9,6 @@ fn main() {
 
     let numero_secreto = rand::thread_rng().gen_range(1, 101);
 
-    println!("O número secreto é: {}", numero_secreto);
-
     loop {
         println!("Digite o seu palpite.");
 
@@ -19,13 +17,15 @@ fn main() {
         io::stdin().read_line(&mut palpite)
             .expect("Falha ao ler entrada");
 
-        let palpite: u32 = palpite.trim().parse()
-            .expect("Por favor, digite um número!");
+        let palpite: u32 = match palpite.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("Você disse: {}", palpite);
 
         match palpite.cmp(&numero_secreto) {
-            Ordering::Less => println!("Muito baixo"),
+            Ordering::Less => println!("Muito baixo!"),
             Ordering::Greater => println!("Muito alto!"),
             Ordering::Equal => {
                 println!("Você acertou!");
